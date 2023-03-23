41ad4e7ea3d7e2b99b757ce0d3b6ba5870e4aab0
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-03-23 04:06:01 -0500

    feat: :zap: Arreglo de ejercicio 5

--- ejercicio05.php
+++ ejercicio05.php
@@ -12,28 +12,24 @@
     //condicionales
     $hora = date('H');
 
-
-    $usuario = "roo";
-    $clave = "123   5";
-    if ($usuario == "root" and $clave == "12345") {
-        echo "";
-        echo "<div class='position-relative'>
-            <h2 class='text text-primary'>Sea Usted bienbenido usuario $usuario <br></h2>
-            <div class='position-absolute top-0 start-50 translate-middle-x'>
-                <img src='img/Logo-UTS-1.png' alt='' class='w-50 '>
-            </div>
-        </div>";
-    }elseif($usuario != "root" and $clave != "12345"){
-        echo "El usuario y la contraseñs son incorrectas";
-    }elseif($usuario != "root"){
-        echo "El usuario es Incorrecto";
-    }else {
-        echo "La contraseña es Incorrecta";
-    }
-
     if ($hora > 13 and $hora < 18){
-       
-
+        $usuario = "roo";
+        $clave = "123   5";
+        if ($usuario == "root" and $clave == "12345") {
+            echo "";
+            echo "<div class='position-relative'>
+                <h2 class='text text-primary'>Sea Usted bienbenido usuario $usuario <br></h2>
+                <div class='position-absolute top-0 start-50 translate-middle-x'>
+                    <img src='img/Logo-UTS-1.png' alt='' class='w-50 '>
+                </div>
+            </div>";
+        }elseif($usuario != "root" and $clave != "12345"){
+            echo "El usuario y la contraseñs son incorrectas";
+        }elseif($usuario != "root"){
+            echo "El usuario es Incorrecto";
+        }else {
+            echo "La contraseña es Incorrecta";
+        }
     }else{
         echo "<p class='text text-danger'> Lo siento pero no puedes ingresar en esta hora. Intentalo mas adelante</p>";
     }
